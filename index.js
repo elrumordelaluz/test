26b211d6b32e0413266081be444fbdec849502fe
Lionel Tzatzkin <elrumordelaluz@hotmail.com>
2019-01-21 23:43:25 +0100

    Add google-photos example

--- index.js
+++
@@ -1,6 +0,0 @@
-const pkg = require('./package.json')
-const format = require('date-fns/format')
-module.exports = {
-  date: (frm = 'MM/DD/YYYY') => format(new Date(), frm),
-  version: pkg.version,
-}
--- test.js
+++
@@ -1,6 +0,0 @@
-import test from 'ava'
-import { date, version } from '.'
-
-test('Returns a string', async t => {
-  await t.true(typeof date() === 'string')
-})
